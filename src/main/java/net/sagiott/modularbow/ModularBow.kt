3fd99ca53bbd57d341d7b944ca113f3c868efbfc
SaGiott <samuelequartuccio@gmail.com>
2023-06-25 21:51:12 +0200

    Initial commit

---
+++ src/main/java/net/sagiott/modularbow/ModularBow.kt
@@ -0,0 +1,59 @@
+package net.sagiott.modularbow
+
+import com.mojang.logging.LogUtils
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.server.ServerStartingEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import thedarkcolour.kotlinforforge.forge.MOD_CONTEXT
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ModularBow.MOD_ID)
+class ModularBow
+{
+    init
+    {
+        val modEventBus = MOD_CONTEXT.getKEventBus()
+
+        modEventBus.addListener()
+        {
+            event: FMLCommonSetupEvent -> commonSetup(event)
+        }
+        MinecraftForge.EVENT_BUS.register(this)
+    }
+
+    private fun commonSetup(event: FMLCommonSetupEvent)
+    {
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    fun onServerStarting(event: ServerStartingEvent?)
+    {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting")
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = [Dist.CLIENT])
+    object ClientModEvents
+    {
+        @SubscribeEvent
+        fun onClientSetup(event: FMLClientSetupEvent?)
+        {
+
+        }
+    }
+
+    companion object
+    {
+        // Define mod id in a common place for everything to reference
+        const val MOD_ID = "modularbow"
+        private val LOGGER = LogUtils.getLogger()
+    }
+}
